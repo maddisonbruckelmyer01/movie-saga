238f44f5d3954a556906b987ece1c64603ea0d40
Maddison Bruckelmyer <maddisonbruckelmyer@Maddisons-MacBook-Air.local>
2019-09-01 11:52:42 -0500

    adds poster route

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import './Poster.css';
-
+import {connect} from 'react-redux';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { withRouter } from "react-router";
 
 class Poster extends Component {
+
+    handleClick = () => {
+        this.props.history.push('/genres')
+    }//end handleClick
+
     render() {
         return(
             <div className="poster">
-                <p><img src={this.props.poster} /></p>
+                <p><img src={this.props.poster} onClick={this.handleClick}/></p>
             </div>
         )
     }
 }
 
-export default Poster;
+export default withRouter(connect()(Poster));
